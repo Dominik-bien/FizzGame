66ccfb1f60a2fe1985e6cd979096928acfa53e54
dombien <dombien23@gmail.com>
2025-02-23 15:40:51 +0100

    Create FizzBuzz program

---
+++ FizzGame/FizzBuzz.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+    public class FizzBuzz
+    {
+        public string PlayFizzBuzz(int number) // Metoda instancyjna (bez static)
+        {
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
+            else if (number % 3 == 0)
+            {
+                return "Fizz";
+            }
+            else if (number % 5 == 0)
+            {
+                return "Buzz";
+            }
+            else
+            {
+                return number.ToString();
+            }
+        }
+    }
---
+++ FizzGame/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+
+            if (int.TryParse(Console.ReadLine(), out int number))
+            {
+                var fizzbuzz = new FizzBuzz();
+                string result = fizzbuzz.PlayFizzBuzz(number);
+                Console.WriteLine("Result: " + result);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+            }
+        }
+    }
+}
